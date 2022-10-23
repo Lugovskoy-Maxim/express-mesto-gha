b6e0a7730a84c4770decb14fc115fde996430c3b
Максим Луговской <lugovskou.myu@yandex.ru>
2022-10-23 13:54:30 +0500

    add: userUpdate

--- controllers/users.js
+++ controllers/users.js
@@ -2,22 +2,57 @@ const User = require("../models/users");
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-  .then((user) => res.send({ data: user }))
-  .catch(err => res.status(500).send({massage : 'Error type:', err}))
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ massage: "Error type:", err }));
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-  .then((user) => {
-    res.status(200).send(user)
-  })
-  .catch((err) =>
-  res.status(500).send({massage : 'Error type:', err}))
+    .then((user) => {
+      res.status(200).send(user);
+    })
+    .catch((err) => res.status(500).send({ massage: "Error type:", err }));
 };
 
 module.exports.findUserbyId = (req, res) => {
   User.findById(req.params.id)
-  .then((user) => res.send({ data: user }))
-  .catch(err => res.status(500).send({massage : 'Error type:', err}))
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ massage: "Error type:", err }));
+};
+
+module.exports.updateUser = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: true, runValidators: true }
+  )
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === "CastError") {
+        throw new BadRequestError("Передан некорректный id");
+      }
+      if (err.name === "ValidationError") {
+        throw new BadRequestError("Неверено задано одно из полей");
+      }
+    });
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    { new: true, runValidators: true }
+  )
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === "CastError") {
+        throw new BadRequestError("Передан некорректный id");
+      }
+      if (err.name === "ValidationError") {
+        throw new BadRequestError("Ошибка в ссылке на новый аватар");
+      }
+    });
 };
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const {
-  getUsers,
-  createUser,
-  findUserbyId,
-} = require('../controllers/users');
+const { getUsers, createUser, findUserbyId, updateUser, updateAvatar } = require("../controllers/users");
 
-router.get('/users', getUsers);
-router.get('/users/:id', findUserbyId);
-router.post('/users', createUser);
+router.get("/users", getUsers);
+router.get("/users/:id", findUserbyId);
+router.post("/users", createUser);
+router.patch("/users/me", updateUser);
+router.patch("/users/me/avatar", updateAvatar);
 
 module.exports = router;

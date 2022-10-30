e219d68e3993aea218d45e5ff141e10359e4ee51
Максим Луговской <lugovskou.myu@yandex.ru>
2022-10-30 23:56:31 +0500

    add error 409

--- controllers/users.js
+++ controllers/users.js
@@ -34,6 +34,10 @@ module.exports.createUser = (req, res, next) => {
         res.status(400).send({ message: err.message });
         return;
       }
+      if (err.code === 11000) {
+        res.status(409).send({ message: err.message });
+        return;
+      }
       next(err);
     })
     .catch(next);

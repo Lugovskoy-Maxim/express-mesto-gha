e0e296fb99c3f1ec80bf6b46f2f1a91dffa8a663
Максим Луговской <lugovskou.myu@yandex.ru>
2022-10-31 09:47:34 +0500

    test 401 error

--- models/users.js
+++ models/users.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-
+const { AuthError } = require('../errors/errorAuth');
 // const { Schema } = mongoose;
 
 const userSchema = new mongoose.Schema(
@@ -43,12 +43,12 @@ const userSchema = new mongoose.Schema(
 userSchema.statics.findUserByCredentials = function validateReq(email, password) {
   return this.findOne({ email }).select('+password').then((user) => {
     if (!user) {
-      return Promise.reject(new Error('Неправильные почта или пароль'));
+      return Promise.reject(new AuthError('Неправильные почта или пароль'));
     }
 
     return bcrypt.compare(password, user.password).then((matched) => {
       if (!matched) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
+        return Promise.reject(new AuthError('Неправильные почта или пароль'));
       }
 
       return user;

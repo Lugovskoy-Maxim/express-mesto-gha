0a307074db6beaab14f5d483a091dec81682906f
Максим Луговской <lugovskou.myu@yandex.ru>
2022-11-16 22:22:36 +0500

    add: CROS

--- app.js
+++ app.js
@@ -8,11 +8,37 @@ const routesUser = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const auth = require('./middlewares/auth');
 const { validateLogin, validateRegister } = require('./middlewares/validation');
-const NotFoundError = require('./errors/NotFoundError'); // 404
+const NotFoundError = require('./errors/NotFoundError');
+// 404
+const allowedCors = [ // список разрешенных адресов
+  'https://api.lugo.nomoredomains.icu',
+  'http://api.lugo.nomoredomains.icu',
+  'https://lugo.nomoredomains.icu',
+  'http://lugo.nomoredomains.icu',
+  'localhost:3000',
+];
 
 const { PORT = 3000, MANGO_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
 mongoose.connect(MANGO_URL);
 const app = express();
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+  }
+
+  next();
+});
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

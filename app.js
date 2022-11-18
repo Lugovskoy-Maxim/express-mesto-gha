d8ea2f7e60f05b8814b58b0c14ec3a0d80e763c2
Максим Луговской <lugovskou.myu@yandex.ru>
2022-11-18 23:12:47 +0500

    fix: cors

--- app.js
+++ app.js
@@ -17,32 +17,32 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const allowedCors = [ // список разрешенных адресов
-  'https://api.lugo.nomoredomains.icu',
-  'http://api.lugo.nomoredomains.icu',
-  'https://lugo.nomoredomains.icu',
-  'http://lugo.nomoredomains.icu',
-  'localhost:3000',
-];
+// const allowedCors = [ // список разрешенных адресов
+//   'https://api.lugo.nomoredomains.icu',
+//   'http://api.lugo.nomoredomains.icu',
+//   'https://lugo.nomoredomains.icu',
+//   'http://lugo.nomoredomains.icu',
+//   'localhost:3000',
+// ];
 // eslint-disable-next-line consistent-return
-app.use((req, res, next) => {
-  const { origin } = req.headers.referer; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,FETCH';
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
-  }
+// app.use((req, res, next) => {
+//   const { origin } = req.headers.referer; // Сохраняем источник запроса в переменную origin
+//   // проверяем, что источник запроса есть среди разрешённых
+//   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+//   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,FETCH';
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//   }
+//   if (method === 'OPTIONS') {
+//     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     // завершаем обработку запроса и возвращаем результат клиенту
+//     return res.end();
+//   }
 
-  next();
-});
+//   next();
+// });
 // добавить проверку почты на уже созданиые
 app.use('/signin', validateLogin, login);
 app.use('/signup', validateRegister, createUser);

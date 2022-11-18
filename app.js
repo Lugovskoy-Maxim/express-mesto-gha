497835e533192214446b49b6e7c73c6eb69ed39f
Максим Луговской <lugovskou.myu@yandex.ru>
2022-11-18 23:06:32 +0500

    fix: CORS

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
   // проверяем, что источник запроса есть среди разрешённых
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,FETCH';
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
